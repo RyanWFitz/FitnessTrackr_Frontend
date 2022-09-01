a82ca65a2746601415d6492e3923c095fccf159d
bethanyallen42 <bethanyallen42@gmail.com>
2022-09-01 14:55:56 -0600

    debugged a couple of things to get routes running

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,49 @@
 import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-
-import { Home } from "./index";
+import { NavLink, Routes, Route } from "react-router-dom";
+import { Home, Account } from "./index";
 
 const App = () => {
-
-  const [ user, setUser ] = useState(false);
-  const [ token, setToken ] = useState('');
-  const [ activities, setActivities ] = useState([]);
-  const [ routines, setRoutines ] = useState([]);
-  const [ routineActivities, setRoutineActivities ] = useState([]);
+  const [user, setUser] = useState(false);
+  const [token, setToken] = useState("");
+  const [activities, setActivities] = useState([]);
+  const [routines, setRoutines] = useState([]);
+  const [routineActivities, setRoutineActivities] = useState([]);
 
   return (
+    <main>
+      {/* NavLink example below in react 18 format */}
+      <nav>
+        <NavLink
+          to="/account"
+          style={(isActive) => ({
+            color: isActive ? "blue" : "red",
+          })}
+        >
+          Account
+        </NavLink>
 
-  <main>
-    {/* NavLink example below in react 18 format */}
-    <NavLink
-      to="/account"
-      style={isActive => ({
-        color: isActive ? "blue" : "red"
-    })}
-    ></NavLink>
+        <NavLink
+          to="/"
+          style={(isActive) => ({
+            color: isActive ? "blue" : "red",
+          })}
+        >
+          Home
+        </NavLink>
+      </nav>
 
-    <NavLink
-      to="/"
-      style={isActive => ({
-        color: isActive ? "blue" : "red"
-    })}
-    ></NavLink>
-
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-    <Routes>
-      <Route 
-      user={user} setUser={setUser} 
-      token={token} setToken={setToken}
-      path='/account' element={<Account />} />
-    </Routes>
-  </main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          user={user}
+          setUser={setUser}
+          token={token}
+          setToken={setToken}
+          path="/account"
+          element={<Account />}
+        />
+      </Routes>
+    </main>
   );
 };
 
