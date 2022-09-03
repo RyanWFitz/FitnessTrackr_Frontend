b32d0f312d2c15746b10976fc9e53d770b9ace4d
monique <baezamonique01@gmail.com>
2022-09-03 11:15:26 -0600

    apicall with errors in the li

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -11,23 +11,31 @@ import { apiCall } from "../utilities/api";
         - be shown an error if the activty already exists
 
 */
-const activities = () => {
+const activities = ({activities, setActivites, user, token}) => {
   
  const getActivities = async () => {
     const fetched = await apiCall('/activites', 'GET', token)
-    setActivites()
+    setActivites(fetched.data.activities)
+    console.log(fetched.data.activities)
  }
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
 
     useEffect(() => {
         getActivities();
-    })
+    }, [])
 
     return <>
         <div>
             <h1> Activities </h1>
         </div>
+        <ul className="activitiesBody">
+            <li className="indActivities" key={activites.id}>
+                <div> Activity ID: {activities.id}</div>
+                <div> Activity: {activities.name}</div>
+                <div> Description: {activities.description}</div>
+            </li>
+        </ul>
 
     </>
        
