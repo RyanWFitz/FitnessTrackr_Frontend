6f7859190c3b72b28b33c34f95f14d9f110daafc
ryanfitz <1ryanfitz@gmail.com>
2022-09-06 11:21:16 -0700

    finished activities, changing branch

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -11,40 +11,81 @@ import { apiCall } from "../utilities/api";
 - be shown an error if the activty already exists
 */
 const activities = ({activities, setActivities, user, token}) => {
+    
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
 
-const getActivities = async () => {
-const fetched = await apiCall('/activities', 'GET', null)
-setActivities(fetched)
-console.log("activities constant", activities)
-}
-const [name, setName] = useState('');
-const [description, setDescription] = useState('');
+    const getActivities = async () => {
+        const fetched = await apiCall('/activities', 'GET', null)
+        setActivities(fetched)
+    // console.log("activities constant", activities)
+    }
 
-useEffect(() => {
-getActivities();
-}, [])
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const newActivity = await apiCall('activities', "POST", token, {name, description} )
+        getActivities()
+        setName('')
+        setDescription('')
+        return newActivity
+    }
 
-return (
-<>
+    useEffect(() => {
+        getActivities();
+    }, [])
 
-<div>
-<h1> Activities </h1>
-</div>
+    return (
+    <>
 
-<div>
-{activities.map( activity => {
-return(
+        <div>
+            <h1> Activities </h1>
+        </div>
 
-<div key={activity.id}>
-<p>Name:{activity.name}</p>
-<p>Description:{activity.description}</p>
-</div>
-);
-})}
-</div>)
-</>
-);
+        <div>
+            {activities.map( activity => {
+                return(
+                    <div key={activity.id}>
+                    <p>Name:{activity.name}</p>
+                    <p>Description:{activity.description}</p>
+                </div>
+                );
+            })}
+        </div>
 
-}
+    {/* 
+        create a handle submit function *
+        call the createActivity api *
+        form needs to have the NAME and DESCRIPTION *
+        show an error if the activity exists, check for the NAME*/}
+        {user && token && (
+            // add onSubmit for the function
+            <form className="item-submit" onSubmit={(e) => {handleSubmit(e)}}>
+                <div>Create Activity</div>
+                <input
+                className="input-text"
+                onChange={(e)=> setName(e.target.value)}
+                type='text' name= 'activityName'
+                placeholder="Required"
+                value={name}
+                required
+                />
+
+                <div>Description</div>
+                <input 
+                className="input-text"
+                onChange={(e) => setDescription(e.target.value)}
+                type='text' name='itemDescription'
+                placeholder="Required"
+                value={description}
+                required
+                />
+                <button className="button" type="submit">Submit Activity</button>
+            </form>        
+        )}
+    </>
+
+    );
+
+    }
 
 export default activities;
